6a76d7a1eb6c862f0ce8412f6eab112f7ac93386
dancsmshenry <1175152442@qq.com>
2022-03-29 14:58:51 +0800

    Add files via upload

--- Leetcode/445.两数相加II.cpp
+++ Leetcode/445.两数相加II.cpp
@@ -52,3 +52,55 @@ public:
         return result -> next;
     }
 };
+
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        stack<int> s1, s2, s3;
+
+        while (l1) {
+            s1.push(l1 -> val);
+            l1 = l1 -> next;
+        }
+        while (l2) {
+            s2.push(l2 -> val);
+            l2 = l2 -> next;
+        }
+
+        int count = 0;
+        while (!s1.empty() || !s2.empty() || count != 0) {
+            int sum = count;
+            if (!s1.empty()) {
+                sum += s1.top(); 
+                s1.pop();
+            }
+            if (!s2.empty()) {
+                sum += s2.top(); 
+                s2.pop();
+            }
+            s3.push(sum % 10);
+            count = sum / 10;
+        }
+
+        ListNode *dummy = new ListNode(0), *res = dummy;
+        while (!s3.empty()) {
+            res -> next = new ListNode(s3.top());
+            res = res -> next;
+            s3.pop();
+        }
+
+        return dummy -> next;
+    }
+};
--- Leetcode/86.分隔链表.cpp
+++ Leetcode/86.分隔链表.cpp
@@ -11,27 +11,27 @@
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
-        /*找到第一个大于x的元素的位置*/
-        ListNode *node1 = nullptr, *node2 = new ListNode(x - 1, head), *result = node2;
-        while (node2 && node2 -> val < x){
-            node1 = node2;
-            node2 = node2 -> next;
-        }        
-
-        ListNode *node3 = node2, *pre = new ListNode(0, node2);
-        while (node3){
-            if (node3 -> val < x){
-                pre -> next = node3 -> next;
-                node1 -> next = node3;
-                node1 = node1 -> next;
-                node3 -> next = node2;
-                node3 = pre -> next;
-            }else{
-                pre = pre -> next;
-                node3 = node3 -> next;
-            }
+        if (!head || !head -> next) {
+            return head;
         }
         
-        return result -> next;
+        ListNode *dummy = new ListNode(x - 1, head), *node = dummy;
+        ListNode *big = new ListNode(0, nullptr), *b = big;
+
+        while (node -> next) {
+            if (node -> next -> val < x) {
+                node = node -> next;
+            } else {
+                ListNode *mid = node -> next -> next;
+                b -> next = node -> next;
+                node -> next = mid;
+                b = b -> next;
+                b -> next = nullptr;
+            }
+        }
+
+        node -> next = big -> next;
+
+        return dummy -> next;
     }
 };
